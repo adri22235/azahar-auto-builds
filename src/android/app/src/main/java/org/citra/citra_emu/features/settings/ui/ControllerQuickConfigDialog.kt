Lime3DS/Lime3DS

extract strings

src/android/app/src/main/java/io/github/lime3ds/android/features/settings/ui/ControllerQuickConfigDialog.kt
@@ -47,9 +47,9 @@ class ControllerQuickConfigDialog(
         val builder: AlertDialog.Builder = AlertDialog.Builder(context)
         builder
             .setView(quickConfigBinding.root)
-            .setTitle("Quick Configure")
-            .setPositiveButton("Next") {_,_ -> }
-            .setNegativeButton("Close") { dialog, which ->
+            .setTitle(context.getString(R.string.controller_quick_config))
+            .setPositiveButton(context.getString(R.string.controller_quick_config_next)) {_,_ -> }
+            .setNegativeButton(context.getString(R.string.controller_quick_config_close)) { dialog, which ->
                 dialog.dismiss()
             }
 

adri22235/azahar-auto-builds

src/android/app/src/main/java/org/citra/citra_emu/features/settings/ui/ControllerQuickConfigDialog.kt
@@ -47,9 +47,9 @@ class ControllerQuickConfigDialog(
         val builder: AlertDialog.Builder = AlertDialog.Builder(context)
         builder
             .setView(quickConfigBinding.root)
-            .setTitle("Quick Configure")
-            .setPositiveButton("Next") {_,_ -> }
-            .setNegativeButton("Close") { dialog, which ->
+            .setTitle(context.getString(R.string.controller_quick_config))
+            .setPositiveButton(context.getString(R.string.next)) {_,_ -> }
+            .setNegativeButton(context.getString(R.string.close)) { dialog, which ->
                 dialog.dismiss()
             }
 