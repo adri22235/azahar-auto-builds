a5ea4d4c2746e71550144aa401400ca39f42aa05
RedBlackAka <140876408+RedBlackAka@users.noreply.github.com>
2026-01-28 08:28:43 +0100

    Please

--- src/citra_qt/configuration/configure_hotkeys.cpp
+++ src/citra_qt/configuration/configure_hotkeys.cpp
@@ -98,7 +98,7 @@ void ConfigureHotkeys::Configure(QModelIndex index) {
     }
     const auto [key_sequence_used, used_action] = IsUsedKey(key_sequence);
 
-    // Check for turbo/framerate limit conflict. Needed to prevent the user from binding both 
+    // Check for turbo/framerate limit conflict. Needed to prevent the user from binding both
     // hotkeys to the same action, which causes problems when resetting the frame limit to the
     // initital value
     const QString current_action =
